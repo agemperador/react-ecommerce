036565096c9ee5fa8a8dad93f353139cee4dc905
aemperador <emperadoragustin@gmail.com>
2020-05-30 23:51:58 -0300

    shop page created and styled with components collection and collection item

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,9 @@ import {Switch, Route} from 'react-router-dom'
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
 
-const HatsPage = ()=>(
 
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
 
 
 function App() {
@@ -17,7 +13,8 @@ function App() {
     <div >
       <Switch>
         <Route exact path='/' component = {HomePage}/>
-        <Route path='/shop/hats' component = {HatsPage}/>
+        <Route path='/shop' component = {ShopPage}/>
+
       </Switch>
     </div>
   );
