a02d9e77ba7c346213a4b88d9f670f98f3d83919
aemperador <emperadoragustin@gmail.com>
2020-07-06 16:29:44 -0300

    added addItem and removeItem functionality to checkout component

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import {connect} from 'react-redux'
+
+import {clearItemFromCart, removeItem, addItem} from '../../redux/cart/cart.actions';
 
 import './checkout-item.styles.scss'
 
 
-const CheckoutItem = ({cartItem:{name,imageUrl,price,quantity}}) =>{
+const CheckoutItem = ({cartItem, clearItem, removeItem, addItem}) =>{
+
+    const  {name,imageUrl,price,quantity} = cartItem;
 
     return(
         <div className='checkout-item'>
@@ -11,11 +16,21 @@ const CheckoutItem = ({cartItem:{name,imageUrl,price,quantity}}) =>{
                 <img src={imageUrl} alt="item"/>
             </div>
             <span className='name'>{name}</span>
-            <span className='quantity'>{quantity}</span>
+            <span className='quantity'>
+                <div className='arrow' onClick={()=>removeItem(cartItem)}>&#10094;</div>
+                <span className='value'>{quantity}</span>
+                <div className='arrow' onClick={()=>addItem(cartItem)}>&#10095;</div>
+            </span>
             <span className='price'>{price}</span>
-            <div className='remove-button'>&#9876;</div>
+            <div className='remove-button' onClick={()=> clearItem(cartItem)}>&#9876;</div>
         </div>
     )
 }
 
-export default CheckoutItem;
+const mapDispatchToProps = dispatch=>({
+    clearItem: item =>dispatch(clearItemFromCart(item)),
+    removeItem: item=>dispatch(removeItem(item)),
+    addItem: item =>dispatch(addItem(item))
+})
+
+export default connect(null,mapDispatchToProps)(CheckoutItem);
--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -3,6 +3,8 @@ import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component
 const CartActionTypes = {
     TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN',
     ADD_ITEM: 'ADD_ITEM',
+    REMOVE_ITEM: 'REMOVE_ITEM',
+    CLEAR_ITEM_FROM_CART: 'CLEAR_ITEM_FROM_CART'
 }
 
 export default CartActionTypes
